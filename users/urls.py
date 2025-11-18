5e2cc172e6d7130e614905976b368c48d8379e06
Sodiq Oydinov <sodiqdevpython@gmail.com>
2025-11-18 20:34:35 +0500

    refresh token qo'shildi

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from rest_framework_simplejwt.views import TokenBlacklistView
+from rest_framework_simplejwt.views import TokenBlacklistView, TokenRefreshView
 
 urlpatterns = [
     path('sign-up/', views.SignUpView.as_view(), name='sign-up'),
@@ -9,5 +9,6 @@ urlpatterns = [
     path('change-main-info/', views.ChangeUserMainInfoView.as_view(), name='change-main-info'),
     path('finish-profile/', views.ChangeProfilePicture.as_view(), name='finish-profile'),
     path('login/', views.LoginView.as_view(), name='login'),
+    path('token-refresh/', TokenRefreshView.as_view()),
     path('logout/', TokenBlacklistView.as_view(), name='token_blacklist'),
 ]
